557daccdf204fc03009409bf806a9751779f2760
ELLEONEL10 <fadiabbara910@gmail.com>
2025-12-07 16:37:02 +0100

    logo

--- backend/build.sh
+++ backend/build.sh
@@ -8,5 +8,9 @@ pip install -r requirements.txt
 # Convert static asset files
 python manage.py collectstatic --no-input
 
-# Apply any outstanding database migrations
+# Apply database migrations
 python manage.py migrate
+
+# Create superuser (or ignore if it already exists)
+# The "|| true" part prevents the build from crashing if the admin already exists
+python manage.py createsuperuser --noinput || true
